86c01e751cc99ece593d005b1f21b40a0f57aab7
Sumit19D <sumitdorugade1@gmail.com>
2026-01-16 19:47:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources into"
-  type        = string
-  default     = "us-east-1"
+  description = "AWS region where resources will be created"
+  type = string
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
